e1786457267af29f22e626ab605116ab0c0082f6
Juan C. Caicedo <7527135+jccaicedo@users.noreply.github.com>
2023-10-11 18:04:21 -0400

    Predictions complete

---
+++ microdet/training_model.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import torch
+import mnmodel
+
+# In[3]:
+
+
+PATCH_SIZE = 256
+STRIDE = 8
+FEATURE_SIZE = 384
+TOKENS_PER_PATCH = PATCH_SIZE // STRIDE
+DIRECTORY = "/home/caicedo/scr/jcaicedo/Micronuclei-data/"
+
+EPOCHS = 20
+BATCH_SIZE = 48
+LR = 0.01
+
+device = 'cuda:2' if torch.cuda.is_available() else 'cpu'
+
+
+# In[4]:
+
+
+filelist = os.listdir(DIRECTORY)
+annot_files = [x for x in filelist if x.endswith('png')]
+
+
+# In[5]:
+
+
+for i in range(len(annot_files)):
+
+    # Leave-one-out split
+    training_files = annot_files.copy()
+    validation_files = [annot_files[i]]
+    del training_files[i]
+    
+    print(" *** ", validation_files, " *** ")
+
+    # Create model
+    model = mnmodel.MicronucleiModel(DIRECTORY, training_files, validation_files, device)
+    
+    # Train
+    model.train(EPOCHS, BATCH_SIZE, LR)
+    
+    # Validate
+    model.validate()
+    
+    # Save
+    model.save()
+
+
+
+
+
+
