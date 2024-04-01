682851348e4cee40288aba077b470a2ff4a21572
任怡帆 <renyifan@nins-MacBook-Air.local>
2024-04-01 11:04:26 -0500

    changed configuration generation file

--- microdet/generate_config.py
+++ microdet/generate_config.py
@@ -4,7 +4,7 @@
 
 import os
 
-loss = ['dice', 'sigmoid_cross_entropy', 'focal']
+loss = ['dice', 'focal', 'sigmoid_cross_entropy']
 LR = [0.1 / (10 ** i) for i in range(5)]
 batch_size = [8, 16, 32]
 train_mode = [True, False]
