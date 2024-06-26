be04bf662ffd5ddef970f1892841350e249094ee
Vidit Agrawal <vagrawal22@wisc.edu>
2024-06-26 10:04:03 -0500

    removed crops

--- cellpose/preprocess.py
+++ cellpose/preprocess.py
@@ -1,3 +1,8 @@
+import skimage
+import glob
+import os
+from PIL import Image
+import numpy as np
 import glob
 import cv2
 import os
@@ -28,5 +33,36 @@ def divide_image(folder_path, save_path, extension):
                 print(f"Saved {save_filepath}")
 
 # Run for different extensions
-divide_image(folder_img_path, save_path, ".png")
-divide_image(folder_img_path, save_path, ".tif")
+#divide_image(folder_img_path, save_path, ".png")
+#divide_image(folder_img_path, save_path, ".tif")
+
+
+'''
+This function will look at all the images present in the following function and remove
+all the images that are black. This is done because the images that are black are not
+useful for training the model.
+'''
+
+folder_path = "/scr/vidit/dataset_nuclei_division"
+
+def remove_empty_images(folder_path):
+    counter = 0
+    for image in glob.glob(folder_path + "/*.png"):
+        img = Image.open(image)
+        #print(image)
+        if not img.convert('L').getbbox():
+            os.remove(image)
+            counter += 1
+            image = image.replace("_outlines", "")
+            image = image.replace(".png", ".tif")
+            os.remove(image)
+            image = image.replace("phenotype", "nuclei-clean")
+            os.remove(image)
+    print(f"Total number of images removed: {counter}")
+
+
+def join_masks(png_file, tif_file):
+    png_img = cv2.imread(png_file)
+    tif_img = cv2.imread(tif_file)
+
+    
