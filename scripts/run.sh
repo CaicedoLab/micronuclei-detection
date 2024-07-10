037e8883bd0d8b2af843a1b77acf43f76676205d
Juan C. Caicedo <7527135+jccaicedo@users.noreply.github.com>
2024-07-10 16:17:07 -0500

    Reorganized code

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,29 @@
+# Label objects and pack results
+label_and_pack()
+{
+    P=$1
+    python masks.py plate_"$P";
+    cd plate_"$P"
+    zip -r masks.zip masks && rm -Rf masks &
+    zip -r prob-maps.zip prob-maps && rm -Rf prob-maps
+    cd ../
+}
+
 # Uncompress outputs
 for k in {1..8}; do 
 	cd plate_"$k" && for j in *zip; do unzip $j& done && cd ../; 
+	wait
 done
+
+for k in {1..8}; do
+	label_and_pack $k &
+done
+wait
 
 # Count and report files
-for k in *; do 
+for k in plate_*; do 
 	n=$(ls $k/output/ | wc -l); 
 	echo $k $n; 
-done
-
-# Find objects and pack results
-for k in {1..8}; do
-	python masks.py plate_"$k";
-	cd plate_"$k"
-	zip -r masks.zip masks && rm -Rf masks &
-	zip -r prob-maps.zip prob-maps && rm -Rf prob-maps
-       cd ../
 done
 
 # Upload results
@@ -25,4 +33,3 @@ for k in {1..8}; do
 	gcloud storage cp prob-maps.zip gs://lasagna-emil/genome-wide_MN_screen/gws_MN_screen_processed_data/plate_"$k"/
 	cd ../
 done
-
