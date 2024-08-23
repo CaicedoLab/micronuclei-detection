e3fa74807511ac706cbfea2e91205a2a099444db
Ren <yren@caicedo-gpu-1.morgridge.net>
2024-08-22 21:01:58 -0500

    modified prediction.py to predict images without ground truth

--- microdet/prediction.py
+++ microdet/prediction.py
@@ -20,7 +20,7 @@ import mnmodel
 import evaluation
 
 CURRENT_PATH = os.getcwd()
-DIRECTORY = CURRENT_PATH + '/dataset_v2'
+DIRECTORY = CURRENT_PATH + '/plate_1_missing'
 OUTPUT_DIR = "/model_output/output/"
 
 # set CHTC writeable cahce directory for pytorch and matplotlib
@@ -46,48 +46,48 @@ BATCH_SIZE = 32
 FINETUNE = True
 WEIGHT_DECAY = 1e-6
 
-
-# set CHTC writeable cahce directory for pytorch and matplotlib
-os.environ['TORCH_HOME'] = CURRENT_PATH + '/.cache/torch'
-os.environ['MPLCONFIGDIR'] = CURRENT_PATH + '/.cache/matplotlib/config'
-
-if len(sys.argv) < 2:
-    print("Use: prediction.py gpu")
+if len(sys.argv) < 3:
+    print("Use: prediction.py imidx gpu")
     sys.exit()
 
 
-# i = int(sys.argv[1]) # which image # for loops to determine i
-gpu = sys.argv[1] # which gpu
+i = int(sys.argv[1])
+gpu = sys.argv[2] # which gpu
 device = f"cuda:{gpu}" if torch.cuda.is_available() else 'cpu'
 
 # avoid files starting with . when untarring in CHTC
 files = os.listdir(DIRECTORY)
 filelist = [file for file in files if not file.startswith('.')]
-annot_files = [x for x in filelist if x.endswith('png')]
-annot_files.sort()
+# annot_files = [x for x in filelist if x.endswith('png')]
+# annot_files.sort()
 
 # Validate
 predictions_dir = DIRECTORY + OUTPUT_DIR
 models_dir = OUTPUT_DIR
 
+# Predict images without ground truth, comment out code of prediciting with ground truth
+
 # Select image for analysis
-validation_file = annot_files[i]
+# validation_file = annot_files[i]
+validation_file = filelist[i]
 imid = validation_file.split('.')[0]
 
 # Load image and annotations
 im = mnds.read_image(DIRECTORY, imid, 'phenotype.tif', scale=SCALE_FACTOR)
 im = np.array((im - np.min(im))/(np.max(im) - np.min(im)), dtype="float32")
-mn_gt = mnds.read_micronuclei_masks(DIRECTORY, imid, SCALE_FACTOR)
+# mn_gt = mnds.read_micronuclei_masks(DIRECTORY, imid, SCALE_FACTOR) # no ground truth in plates
 
 # Load model and compute probabilities
 model = mnmodel.MicronucleiModel(DIRECTORY, device, patch_size=PATCH_SIZE, edges=True)
-model.load(validation_file.replace('phenotype_outlines.png','pth'), model_dir=models_dir)
+# model.load(validation_file.replace('phenotype_outlines.png','pth'), model_dir=models_dir)
+model.load('best_model.pth', model_dir=models_dir)
 probabilities = model.predict(im, stride=1, step=STEP, batch_size=BATCH_SIZE)
-filename = predictions_dir + validation_file.replace('phenotype_outlines.png','_probabilities')
+# filename = predictions_dir + validation_file.replace('phenotype_outlines.png','_probabilities')
+filename = predictions_dir + validation_file.replace('phenotype.tif','_probabilities')
 np.save(filename, probabilities)
 
 mn_pred = probabilities[0,:,:] > THRESHOLD
-evaluation.segmentation_report(imid=imid, predictions=mn_pred, gt=mn_gt, intersection_ratio=0.1, report_obj='Micronuclei')
+# evaluation.segmentation_report(imid=imid, predictions=mn_pred, gt=mn_gt, intersection_ratio=0.1, report_obj='Micronuclei')
 
 # release the resources
 torch.cuda.empty_cache()
