1e877bd960aa38f7d2058066b625273ecb91b9d4
Ren <yren@caicedo-wkstn.morgridge.net>
2025-02-17 11:33:11 -0600

    remove masks above 100 to evaluate the performance of predicted micronuclei

--- mnfinder/cellpose_prediction.py
+++ mnfinder/cellpose_prediction.py
@@ -12,7 +12,7 @@ import wandb
 import microdet.evaluation
 import microdet.mnds
 
-ARCHITECTURE = "Cellpose predictions on 47 non-scaled validation images"
+ARCHITECTURE = "Cellpose predictions on 47 nonscaled validation images"
 CURRENT_PATH = os.getcwd()
 DIRECTORY = CURRENT_PATH + '/validation_no_rescale/'
 
@@ -40,7 +40,7 @@ for i in range(len(validation_files)):
         project='Best_Experiment',
         config={
             "architecture":ARCHITECTURE,
-            'model':'Cellpose',
+            'model':'Cellpose cyto3',
             'diameter':DIAM
         },
         name=f'{imid}',
@@ -52,12 +52,25 @@ for i in range(len(validation_files)):
     
     masks = np.asarray(masks, dtype='uint16')
     
-    save_path = DIRECTORY + 'cellpose_predictions_output/'
-    np.save(save_path + imid + '._probabilities.npy', masks)
+    # remove sizes > 100
+    MICRON_AREA_THRESHOLD = 100
+    labels = skimage.morphology.label(masks)
+    micron_labels = []
+    for i in range(1, len(np.unique(labels))):
+        area = np.sum(labels == i)
+        if area < MICRON_AREA_THRESHOLD:
+            micron_labels.append(i)
+            
+    micro_mask = np.zeros_like(masks)
+    for i in micron_labels:
+        micro_mask += (labels == i)
+        
+    save_path = DIRECTORY + 'cellpose_nonscaled_predictions/'
+    np.save(save_path + imid + '._probabilities.npy', micro_mask)
     
     # evaluation
     mn_gt = microdet.mnds.read_image(DIRECTORY, imid, 'phenotype_outlines.png', scale=SCALE_FACTOR)
-    microdet.evaluation.segmentation_report(imid=imid, predictions=masks, gt=mn_gt, intersection_ratio=0.1, report_obj='Micronuclei')
+    microdet.evaluation.segmentation_report(imid=imid, predictions=micro_mask, gt=mn_gt, intersection_ratio=0.1, report_obj='Micronuclei')
 
 # release the resources
 wandb.finish()
