66ace07fbf194d5b43acc567b2d664af2c6df2d4
Ren <yren@caicedo-wkstn.morgridge.net>
2025-08-05 17:06:59 -0500

    restrucred training & prediction files with argument parser for reproducibility

---
+++ prediction.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import time
+import skimage.morphology
+import torch
+import skimage
+import wandb
+from tqdm import tqdm
+import argparse
+import numpy as np
+
+import dinomn.mnds as mnds
+import dinomn.mnmodel as mnmodel
+import dinomn.evaluation as evaluation
+
+
+
+if __name__ == '__main__':
+
+    # set CHTC writeable cahce directory for pytorch and matplotlib
+    os.environ['TORCH_HOME'] = os.getcwd() + '/.cache/torch'
+    torch.set_num_threads(8)
+    
+    parser = argparse.ArgumentParser(
+        description="DinoMN Inference",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter # Shows default values in help message
+    )
+    
+    parser.add_argument('--path', type=str, help='DinoMN dataset path')
+    parser.add_argument('--gpu', type=int, default=0, help='GPU device index.')
+    parser.add_argument('--step', type=int, default=64, help='Step size of prediction box, larger value will decrease inference time but harm accuracy.')
+    parser.add_argument('--batch_size', type=int, default=4, help='Number of crops from one image that are predicted simultaneously.')
+    parser.add_argument('--prob_threshold', type=float, default=0.5, help='Probability threshold to classify if each pixel is micronucleus.')
+    parser.add_argument('--iou_threshold', type=float, default=0.1, help='IOU threshold, 0.1 suggested for subcellular structures.')
+    parser.add_argument('--scale', type=float, default=1.0, help='Scale factor for aligning microscopy magnification.')
+    parser.add_argument('-w', '--wandb_mode', action='store_true', help='Choose to turn on Weights and Biases')
+    
+    # Sample command: python3 prediction.py --path '/scr/yren/annotated_mn_datasets/' --gpu 0 --step 64 --batch_size 4 --prob_threshold 0.5 --iou_threshold 0.1 --scale 1 -w
+    
+    # Fixed Hyperparameters
+    PATCH_SIZE = 256
+    FEATURE_SIZE = 384
+    
+    args = parser.parse_args()
+
+    DIRECTORY = args.path
+    VALIDATION_PATH = os.path.join(DIRECTORY, 'validation/images')
+    MODEL_DIR = "model_output/"
+
+    GPU = args.gpu
+    STEP = args.step # 64 is the best
+    THRESHOLD = args.prob_threshold
+    IoU_THRESHOLD = args.iou_threshold # for micronuclei
+    PREDICTION_BATCH = args.batch_size    
+    SCALE_FACTOR = args.scale
+
+    device = f"cuda:{GPU}" if torch.cuda.is_available() else 'cpu'
+    ARCHITECTURE = f'DinoMN Inference'
+
+
+    # avoid files starting with . when untarring in CHTC
+    files = os.listdir(VALIDATION_PATH)
+    filelist = [file for file in files if not file.startswith('.')]
+    annot_files = filelist.copy()
+    annot_files.sort()
+
+    # Validate
+    predictions_dir = os.path.join(DIRECTORY, MODEL_DIR)
+
+    # Load model and compute probabilities
+    model = mnmodel.MicronucleiModel(
+        device=device,
+        data_dir=DIRECTORY
+    )
+    model_name = 'DinoMN.pth'
+    model.load(os.path.join(predictions_dir, model_name))
+
+
+    for i in tqdm(range(len(annot_files))):
+        validation_file = annot_files[i]
+        imid = validation_file.split('.')[0]
+        
+        config = {
+                "architecture":ARCHITECTURE,
+                "Loss Weight": "all default, sam ratio (0.95focal+0.05dice) + gamma=2, etc",
+                "prediction_batch_size":PREDICTION_BATCH,
+                "scale_factor":'Predict on images that are not scaled',
+                'step':STEP,
+                "feature_size":FEATURE_SIZE,
+                "patch_size":PATCH_SIZE,
+                "probability_threshold":THRESHOLD,
+                "IoU_threshold":IoU_THRESHOLD,
+                "dilation":0,
+                "gaussian":'gaussian not need for prediction',
+                'Number of validation images':len(annot_files)
+            }
+        wandb.init(
+            project='Best_Experiment',
+            config=config,
+            name=f'{imid}',
+            reinit=True
+        )
+        
+        # Load image and annotations
+        im = mnds.read_image(os.path.join(VALIDATION_PATH, validation_file), scale=SCALE_FACTOR)
+        im = np.array((im - np.min(im))/(np.max(im) - np.min(im)), dtype="float32")
+        
+        mn_gt = mnds.read_image(os.path.join(VALIDATION_PATH.replace('images', 'mn_masks'), validation_file.replace('.tif', '.png')), scale=SCALE_FACTOR)
+        mn_gt = mn_gt > 0 # convert to boolean (binary mask)
+        
+        # Document inference time
+        s = time.time()
+        probabilities = model.predict(im, stride=1, step=STEP, batch_size=PREDICTION_BATCH) # has model.eval() & with torch.no_grad()
+        e = time.time()
+        wandb.log({'Inference Time': e-s})
+        filename = os.path.join(DIRECTORY, MODEL_DIR) + imid + '._probabilities'
+        
+        mn_pred = probabilities[0,:,:] > THRESHOLD
+        labeled_mn = skimage.morphology.label(mn_pred)
+        labeled_mn = np.asarray(labeled_mn, dtype='uint16') # if saving as img
+        
+        evaluation.segmentation_report(imid=imid, 
+                                    predictions=labeled_mn, 
+                                    gt=mn_gt, 
+                                    intersection_ratio=IoU_THRESHOLD,
+                                    wandb_mode=True)
+        
+        # save labeled matrices
+        np.save(filename, labeled_mn)
+        
+    # release the resources
+    torch.cuda.empty_cache()
---
+++ training_model.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import torch
+import argparse
+import wandb
+import dinomn.mnmodel as mnmodel
+
+
+
+if __name__ == '__main__':
+    
+    # CURRENT_PATH = os.getcwd()
+    # DIRECTORY = CURRENT_PATH + '/annotated_mn_datasets/'
+    # OUTPUT_DIR = "model_output/"
+    # if not os.path.exists(DIRECTORY + OUTPUT_DIR):
+    #     os.makedirs(DIRECTORY + OUTPUT_DIR)
+
+    # set CHTC writeable cahce directory for pytorch and matplotlib
+    os.environ['TORCH_HOME'] = os.getcwd() + '/.cache/torch'
+    torch.set_num_threads(8)
+
+    parser = argparse.ArgumentParser(
+        description="DinoMN Training",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter # Shows default values in help message
+    )
+
+    parser.add_argument('--path', type=str, help='Micronuclei dataset path')
+    parser.add_argument('--gpu', type=int, default=0, help='GPU device index.')
+    parser.add_argument('--epochs', type=int, default=20, help='Number of training epochs.')
+    parser.add_argument('--batch_size', type=int, default=4, help='Training batch size.')
+    parser.add_argument('--loss_fn', type=str, default='combined', choices=['dice', 'focal','combined'], help='Loss function.')
+    parser.add_argument('--lr', type=float, default=1e-5, help='Learning rate for the optimizer.')
+    parser.add_argument('--weight_decay', type=float, default=1e-6, help='Weight decay for the optimizer.')
+    parser.add_argument('--scale', type=float, default=1.0, help='Scale factor for aligning microscopy magnification.')
+
+    parser.add_argument('--finetune', action='store_true', help='Finetuning the DINOv2 backbone.')
+    parser.add_argument('--gaussian', action='store_true', help='Gaussian random size crop for model to learn distortion.')
+    parser.add_argument('--edges', action='store_true', help='Recover object edges in training.')
+    parser.add_argument('-w', '--wandb_mode', action='store_true', help='Choose to turn on Weights and Biases')
+    parser.set_defaults(finetune=True, gaussian=True, edges=False, wandb_mode=False)
+
+    # Sample command: python3 training_model.py --path '/scr/yren/annotated_mn_datasets/' --gpu 0 --epochs 20 --loss_fn 'combined' --lr 1e-6 --scale 1.0 --finetune --gaussian --wandb_mode
+    
+    # Fixed value for DINOv2
+    PATCH_SIZE = 256
+    FEATURE_SIZE = 384
+    
+    args = parser.parse_args()
+
+    DIRECTORY = args.path
+    OUTPUT_DIR = "model_output/"
+    if not os.path.exists(DIRECTORY + OUTPUT_DIR):
+        os.makedirs(DIRECTORY + OUTPUT_DIR)
+        
+    GPU = args.gpu
+    EPOCHS = args.epochs
+    BATCH_SIZE = args.batch_size # best training batch size
+    LOSS_FN = args.loss_fn
+    LR = args.lr
+    WEIGHT_DECAY = args.weight_decay
+    SCALE_FACTOR = args.scale
+    
+    FINETUNE = args.finetune
+    GAUSSIAN = args.gaussian
+    EDGES = args.edges
+    
+    if args.wandb_mode:
+        WANDB_MODE = 'online'
+    else:
+        WANDB_MODE = 'disabled'
+
+    ARCHITECTURE = 'DinoMN Training'
+
+    device = f"cuda:{GPU}" if torch.cuda.is_available() else 'cpu'
+    
+    num_training_files = len(os.listdir(DIRECTORY + 'train/images'))
+    num_validation_files = len(os.listdir(DIRECTORY + 'validation/images'))
+
+
+    config = {
+        "architecture":ARCHITECTURE,
+        "Loss": LOSS_FN,
+        "Loss Weight": "all default, sam ratio (0.95focal+0.05dice) + gamma=2, etc",
+        "fine_tuning":FINETUNE,
+        "training_batch_size":BATCH_SIZE,
+        "start_learning_rate":LR,
+        "lr_scheduler":"Cosine",
+        "scale_factor":'Trained on non-scaled images',
+        "epochs": EPOCHS,
+        "feature_size":FEATURE_SIZE,
+        "patch_size":PATCH_SIZE,
+        "weight_decay":WEIGHT_DECAY,
+        "gaussian":GAUSSIAN,
+        'edges':EDGES,
+        'Number of training images':num_training_files,
+        'Number of validation images':num_validation_files
+    }
+    wandb.init(
+        project='Best_Experiment',
+        config=config,
+        name=f'model version 3 (test new predict())',
+        mode=WANDB_MODE
+    )
+
+    # Create model
+    model = mnmodel.MicronucleiModel(
+        device=device,
+        data_dir=DIRECTORY,
+        patch_size=PATCH_SIZE,
+        scale_factor=SCALE_FACTOR,
+        edges=EDGES, # False, this will recover the input edges, reducing performance
+        gaussian=GAUSSIAN
+    )
+
+    # Train
+    model.train(epochs=EPOCHS, 
+                batch_size=BATCH_SIZE, 
+                learning_rate=LR, 
+                loss_fn=LOSS_FN, 
+                finetune=FINETUNE,
+                weight_decay=WEIGHT_DECAY,
+                wandb_mode=True
+    )
+
+
+    # Save
+    model.save(outdir=OUTPUT_DIR, model_name='DinoMN')
+
+    # release the resources
+    torch.cuda.empty_cache()
+    wandb.finish()
