8b5c12c934088d5a5055ebe0083e02bdeeeb7724
Ren <yren@caicedo-wkstn.morgridge.net>
2025-11-05 09:48:52 -0600

    mnfinder evaluation on our test set experiment

--- baselines/mnfinder_prediction.py
+++ baselines/mnfinder_prediction.py
@@ -2,76 +2,86 @@
 import os
 import sys
 sys.path.append('../')
-from pathlib import Path
 from tqdm import tqdm
+import argparse
 import time
 import numpy as np
-import pandas as pd
-from tifffile import tifffile
-from matplotlib import pyplot as plt
-from PIL import Image
+# import pandas as pd
+# from tifffile import tifffile
 import skimage
-from skimage.measure import label
-from skimage.color import label2rgb
 import wandb
-# import cv2
 
 from skimage.exposure import rescale_intensity
 from skimage.segmentation import clear_border
 
 from mnfinder import MNClassifier
-import dinomn.evaluation as evaluation
-import dinomn.mnds as mnds
-
-ARCHITECTURE = "MNFinder predictions - Inference time"
-CURRENT_PATH = os.getcwd()
-DIRECTORY = CURRENT_PATH + '/all_data_micronuclei_no_rescale/validation/'
+import mndino.evaluation as evaluation
+# import mndino.mnds as mnds
 
 
-files = os.listdir(DIRECTORY)
-filelist = [file for file in files if not file.startswith('.')] # avoid files starting with . when untarring in CHTC
-validation_files = [x for x in filelist if x.endswith('.phenotype.tif')]
-validation_files.sort()
-
-
-key_file = open('/scr/yren/wandb_key.txt', 'r')
-key = key_file.readline()
-wandb.login(key=key)
+if __name__ == '__main__':
     
-attention_model = MNClassifier.get_model('Attention')
-
-for i in range(len(validation_files)):
-    imid = validation_files[i].split('.')[0]
-    
-    SCALE_FACTOR = 1.0
-   
-    wandb.init(
-        project='Best_Experiment',
-        config={
-            "architecture":ARCHITECTURE,
-            'model':'MNFinder',
-            'scale_factor':SCALE_FACTOR
-        },
-        name=f'{imid}',
-        reinit=True
+    parser = argparse.ArgumentParser(
+        description="MNFinder Prediction",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter # Shows default values in help message
     )
-
-    im = mnds.read_image(DIRECTORY, imid, 'phenotype.tif', scale=SCALE_FACTOR)
     
-    # Document inference time
-    s = time.time()
-    labels = attention_model.predict(im)
-    e = time.time()
-    wandb.log({'Inference Time': e-s})
+    parser.add_argument('--path', type=str, help='mnDINO dataset path') # '/scr/yren/annotated_mn_datasets/test/images/'
+    parser.add_argument('--save_path', type=str, help='Path to save MNFinder predictions')
+
+    args = parser.parse_args()
+    PATH = args.path
+    SAVE_PATH = args.save_path
+
+    files = os.listdir(PATH)
+    filelist = [file for file in files if not file.startswith('.')] # avoid files starting with . when untarring in CHTC
+    # validation_files = [x for x in filelist if x.endswith('.phenotype.tif')]
+    filelist.sort()
+        
+    combined_model = MNClassifier.get_model() # default: combined
+
+    ARCHITECTURE = "MNFinder predictions"
+    SCALE_FACTOR = 1.0
     
-    micro_labels = labels[:,:,2]
+    for i in tqdm(range(len(filelist))):
+        imid = filelist[i].split('.')[0]
+    
+        wandb.init(
+            project='mnDINO-experiment',
+            config={
+                "architecture":ARCHITECTURE,
+                'model':'MNFinder',
+                'scale_factor':SCALE_FACTOR
+            },
+            name=f'{imid}',
+            reinit=True
+        )
 
-    save_path = CURRENT_PATH + '/mnfinder_predictions/'
-    np.save(save_path + imid + '._probabilities.npy', micro_labels)
+        im_path = os.path.join(PATH, imid + '.tif')
+        im = skimage.io.imread(im_path)
+        if SCALE_FACTOR != 1.0:
+            im = skimage.transform.rescale(im, scale=SCALE_FACTOR)
+        
+        # Document inference time
+        s = time.time()
+        labels = combined_model.predict(im)
+        e = time.time()
+        wandb.log({'Inference Time': e-s})
+        
+        micro_labels = labels[:,:,1]
 
-    # evaluation
-    mn_gt = mnds.read_image(DIRECTORY, imid, 'phenotype_outlines.png', scale=SCALE_FACTOR)
-    evaluation.segmentation_report(imid=imid, predictions=micro_labels, gt=mn_gt, intersection_ratio=0.1, report_obj='Micronuclei')
+        # save_path = CURRENT_PATH + '/mnfinder_predictions/'
+        
+        np.save(os.path.join(SAVE_PATH, imid + '._probabilities.npy'), micro_labels)
 
-# release the resources
-wandb.finish()
+        # evaluation
+        # mn_gt = mnds.read_image(PATH.replace('images', 'mn_masks'), imid, 'phenotype_outlines.png', scale=SCALE_FACTOR)
+        # MNFinder tensorflow conflicts with torch, do not import mnds file.
+        gt_path = os.path.join(PATH.replace('images', 'mn_masks'), imid + '.png')
+        mn_gt = skimage.io.imread(gt_path)
+        if SCALE_FACTOR != 1.0:
+            mn_gt = skimage.transform.rescale(mn_gt, scale=SCALE_FACTOR)
+        evaluation.segmentation_report(imid=imid, predictions=micro_labels, gt=mn_gt, intersection_ratio=0.1, wandb_mode=True)
+
+    # release the resources
+    wandb.finish()
