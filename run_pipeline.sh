254e6461b8dec75f42325cfd040dacdbe3d2a9df
Ren <yren@caicedo-wkstn.morgridge.net>
2025-12-22 20:31:14 -0600

    reproduce iteration experiments on validation set

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-# for j in {1..10}; do
-    # echo "Running iteration $j"
+for j in {1..10}; do
+    echo "Running iteration $j"
 
-python3 training_model.py \
-    --path '/scr/yren/annotated_mn_datasets/' \
-    --gpu 0 \
-    --epochs 20 \
-    --batch_size 4 \
-    --loss_fn 'combined' \
-    --lr 1e-5 \
-    --scale 1.0 \
-    --gaussian \
-    --wandb_mode \
+    python3 training_model.py \
+        --path '/scr/data/annotated_mn_datasets/' \
+        --gpu 0 \
+        --epochs 20 \
+        --batch_size 4 \
+        --loss_fn 'combined' \
+        --lr 1e-5 \
+        --scale 1.0 \
+        --gaussian \
+        --wandb_mode \
+        --tags "mndino exp on cleaned data - iter $j" \
+        --iteration $j
 
-python3 prediction.py \
-    --path '/scr/yren/annotated_mn_datasets/' \
-    --test_set \
-    --gpu 0 \
-    --step 32 \
-    --batch_size 4 \
-    --prob_threshold 0.5 \
-    --iou_threshold 0.1 \
-    --scale 1 \
-    --wandb_mode \
-
-# done
+    python3 prediction.py \
+        --path '/scr/data/annotated_mn_datasets/' \
+        # --test_set \ run on validation set for model selection
+        --gpu 0 \
+        # --step 32 \ use 64 step for 10 iteration validation set
+        --step 64
+        --batch_size 4 \
+        --prob_threshold 0.5 \
+        --iou_threshold 0.1 \
+        --scale 1 \
+        --wandb_mode \
+        --tags "mndino exp on cleaned data - iter $j" \
+        --iteration $j
+done
